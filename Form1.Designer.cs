90ffc26fb88f7c1d5fa9dc6a07f4be10471cb806
BuiAnh12 <buianh120403@gmail.com>
2024-06-21 23:28:56 +0700

    PATCH: Mering error from TA and add sp_script

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -55,16 +55,12 @@
             this.nhom = new System.Windows.Forms.ToolStripStatusLabel();
             this.xtraTabControl = new DevExpress.XtraTab.XtraTabControl();
             this.ribbonPageGroup5 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
-// <<<<<<< HEAD
 //             this.phieuNhapBtn = new DevExpress.XtraBars.BarButtonItem();
 //             ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
-// =======
             this.chiTietNhanVien = new DevExpress.XtraBars.BarButtonItem();
             ((System.ComponentModel.ISupportInitialize)(this.btnReportCTSL)).BeginInit();
-=========
-            this.phieuNhapBtn = new DevExpress.XtraBars.BarButtonItem();
-            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
->>>>>>>>> Temporary merge branch 2
+//            this.phieuNhapBtn = new DevExpress.XtraBars.BarButtonItem();
+//            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl)).BeginInit();
             this.SuspendLayout();
